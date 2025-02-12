b00b45f9e2668a3bf7e09032430d0961c1166b14
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2025-02-12 12:58:52 +0100

    Changed color palette

--- Scripts/constants.py
+++ Scripts/constants.py
@@ -1,11 +1,11 @@
 # Plot using a color gradient
 colors=(
-    "#003f5c",
-    "#58508d",
-    "#8a508f",
-    "#bc5090",
-    "#de5a79",
-    "#ff6361",
-    "#ff8531",
-    "#ffa600"
+    "red",
+    "orange",
+    "gold",
+    "green",
+    "teal",
+    "blue",
+    "purple",
+    "pink"
 )
