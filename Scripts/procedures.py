ad31034d94e6c815bd01bf07e9b0c8f53ef1ba60
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2025-02-12 15:33:55 +0100

    Adding xy to linear position mapping

--- Scripts/procedures.py
+++ Scripts/procedures.py
@@ -13,5 +13,17 @@ def getxy(linear_position):
 
     return (x,y)
 
+def getlinearpos(x,y):
+    #Position if Y was 0
+
+    #if on even column
+    if x%2==0:
+        linear_position=x*8+y
+    #if on odd column
+    else:
+        linear_position=x*8+(7-y)
+
+    return linear_position
+
 def current_time():
     return time.strftime("%Y%m%d-%H%M%S")
