9a9e89f5384920a4e25583b3173955de41dfccc4
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2025-02-12 10:46:03 +0100

    Loading files

--- Scripts/cv_light_plots.py
+++ Scripts/cv_light_plots.py
@@ -1,2 +1,10 @@
-import numpy
-import matplotlib
+import numpy as np
+import matplotlib as plt
+from glob import glob
+
+data = []
+#Load data - multiple datasets
+for file in glob("../CV Light/TiO2_24_CV_Light(*)",root_dir="."):
+    set=np.loadtxt(file,skiprows=1)
+    data.append(set) # Append all dataset
+
