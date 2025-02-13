825129dabd40f87faaa84e02d0265976d42d251c
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2025-02-13 10:32:04 +0100

    Adding single plot program

---
+++ Scripts/single_plot.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+import time
+import constants as c
+import procedures as proc
+
+data_base = []
+data_normalized = []
+#Load data - multiple datasets
+file= proc.get_sorted_filenames("../CV Light/TiO2_24_CV_Light(*)")[58]
+voltage, we_current, vrhe, specific_current=np.loadtxt(file,skiprows=1,unpack=True)
+
+specific_current=proc.reject_outliers(specific_current,31)
+
+iv_ch={"x":vrhe,"y":specific_current}
+iv_normalized={"x":vrhe,"y":specific_current}
+
+data_base.append(iv_ch) # Append all dataset
+data_normalized.append(iv_normalized)
+
+
+#General plots
+i=0
+for set in data_base:
+    r=i%16
+    if 8<=r<=15:
+        r=7-(r%8)
+
+    plt.plot(set["x"],set["y"],color=c.colors[r],ls="dotted")#,label="Riga {}".format(r+1))
+
+    i=i+1
+
+
+
+
+#Drawing
+plt.legend()
+plt.xlabel("Potential (V)")
+plt.ylabel("WE Current(A)")
+plt.savefig("../Artifacts/CV_Light_"+time.strftime("%Y%m%d-%H%M%S")+".png")
+plt.show()
+
