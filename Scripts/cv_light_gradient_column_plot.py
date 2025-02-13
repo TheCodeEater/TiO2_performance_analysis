7343bbc8333fa8f7ac58e4e71ca1b5f12d35b4a4
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2025-02-13 10:44:11 +0100

    Typos, comments, filepath

--- Scripts/cv_light_gradient_column_plot.py
+++ Scripts/cv_light_gradient_column_plot.py
@@ -21,32 +21,31 @@ for file in proc.get_sorted_filenames("../CV Light/TiO2_24_CV_Light(*)"):
     data_normalized.append(iv_normalized)
 
 
-#Plots
+#Create subplots for each column
 figure, ax = plt.subplots(nrows=2,ncols=4,figsize=(30,15)) #Plot data along cols (contains 8 plots, one for each row of the col assigned to the plot)
 plots=ax.flatten()
 
 
-# Col plots
+# Plot for each column
 for col in range(0,8): #for each column
-    #Get column as lienar array
-    column=data_base[col*8:(col+1)*8]#excludes last element
+    #Get column as linear array
+    column=data_base[col*8:(col+1)*8]#excludes last element, which is part of the next column (or out of bound if last col)
     #Reverse row if parity is odd (first column is 0)
     if col%2==1:
         column=reversed(column)
 
-    i=0
+    row_count=0
     for row in column: #for each row, plot according to colors
-        plots[col].plot(row["x"],row["y"],color=c.colors[i],label="Riga {}".format(i+1))
-        i=i+1
+        plots[col].plot(row["x"],row["y"],color=c.colors[row_count],label="Riga {}".format(row_count+1))
+        row_count+=1
 
+    #Set labels and title
     plots[col].set(xlabel="Potential (V)", ylabel="Current (A)", title="Column {}".format(col + 1))
     plots[col].legend()
 
 
 #Drawing
-#matrix.legend()
-#matrix.set(xlabel="Potential (V)",ylabel="WE Current(A)")
 plt.legend()
-plt.savefig("../Artifacts/CV_Light_"+time.strftime("%Y%m%d-%H%M%S")+".png")
+plt.savefig("../Artifacts/cv_light_column_gradient/CV_Light_COL_"+time.strftime("%Y%m%d-%H%M%S")+".png")
 plt.show()
 
