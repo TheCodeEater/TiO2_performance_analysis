832add9f108c09716a63a3c01a0e243adb8d0234
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2025-02-14 15:10:21 +0100

    Adding comments, better decorator name

--- Scripts/decorators.py
+++ Scripts/decorators.py
@@ -5,12 +5,15 @@ import functools
 #
 
 #Load config here and set constants, to be later used by decorators
+X_points=8
+Y_points=8
 
 #
 # Config decorators
+# Add config entries to kwargs
 #
 
-def LoadConfig(f):
+def LoadMatrixSize(f):
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
         return f(*args, **kwargs)
