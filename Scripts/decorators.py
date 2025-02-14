2b96a103bfbcf89ef51e1004d60f57987d5c9248
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2025-02-14 17:24:58 +0100

    Adding smoothing decorator

--- Scripts/decorators.py
+++ Scripts/decorators.py
@@ -1,4 +1,6 @@
 import functools
+import numpy as np
+import scipy as sp
 
 #
 # Config loader
@@ -23,3 +25,18 @@ def LoadMatrixSize(f):
         return f(*args, **kwargs)
 
     return wrapper
+
+def Smooth(finesseX=200,finesseY=200,s_factor=10):
+    def smooth_decorator(f):
+        @functools.wraps(f)
+        def wrapper(*args,**kwargs):
+            X,Y,Z=f(*args,**kwargs)
+
+            xnew, ynew = np.mgrid[0:X_points-1:finesseX, 0:X_points-1:finesseY]
+            tck = sp.interpolate.bisplrep(X, Y, Z, s=s_factor)
+            znew = sp.interpolate.bisplev(xnew[:, 0], ynew[0, :], tck)
+
+            return (xnew,ynew,znew)
+
+        return wrapper
+    return smooth_decorator
